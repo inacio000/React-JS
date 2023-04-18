8eb8a8431c2b24af1cc84d24365a552383126b07
Inácio Raimundo <84747204+inacio000@users.noreply.github.com>
2023-04-18 17:44:00 +0300

    add modal component using the react-modal package

---
+++ inmoney/src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,12 @@
+import Modal from 'react-modal'
+
+export function NewTransactionModal() {
+    return (
+    //     <Modal 
+    //       isOpen={isNewTransactionModalOpen}
+    //       onRequestClose={handleCloseNewTransactionModal}
+    //   >
+    //       <h2>Register transaction</h2>
+    //   </Modal>
+    )
+}
