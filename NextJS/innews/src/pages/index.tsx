a5b4466642819c5505e675cacfdd2d30882a1dc2
Inácio Raimundo <84747204+inacio000@users.noreply.github.com>
2023-06-17 11:20:43 +0300

    add sass

---
+++ NextJS/innews/src/pages/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "../styles/home.module.scss"
+
+export default function Home() {
+  return (
+    <h1 className={styles.title}>
+      Hello World
+    </h1>
+  )
+}
